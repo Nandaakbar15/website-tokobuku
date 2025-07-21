71a300bed7087be94d022430b3ec04ef2e513950
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-07-21 18:19:38 +0700

    Update package.json

--- frontend/src/pages/dashboard/databuku/indexbuku.jsx
+++ frontend/src/pages/dashboard/databuku/indexbuku.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
